69e462e347d1ee8887e09f376172f01eca53cb26
vBetter7 <145014319+vBetter7@users.noreply.github.com>
2023-11-02 14:02:01 +0100

    Add files via upload

---
+++ 02.11.23/panel/index.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['admin_name'])) {
+    header('Location: ./login.php');
+    die;
+}
+
+$title = 'Panel administracyjny';
+$activePage = 'home';
+?>
+
+<!doctype html>
+<html lang="pl">
+<?php
+require_once '../include/head.php';
+?>
+<body>
+    <div class="container-fluid p-0">
+        <?php
+        require_once './include/nav.php';
+        ?>
+        <div class="container py-5">
+            <h1>Hello panel</h1>
+</div>
+</body>
+</html>
